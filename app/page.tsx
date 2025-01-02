98a25c40e43ab368630228fd7709bcfa9d342b79
Jha <jhabhanup@gmail.com>
2025-01-03 03:35:07 +0530

    tailwind-eslint-font-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div className=" text-3xl font-black  text-white">NextJS App</div>;
-}
+import React from "react";
+import "./globals.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="h1-bold"> Welcome to the world of next.JS</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of next.JS
+      </h1>
+    </>
+  );
+};
+
+export default Home;
